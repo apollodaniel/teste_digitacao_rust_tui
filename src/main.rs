57997aab505f8e7f3bfb786c3dd8ffa34fe42351
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-02-02 00:39:09 +0000

    UI working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    error::Error,
-    io::{stderr, stdout, Write},
+    error::Error, io::{stderr, stdout, Write}, rc::Rc
 };
 
 use crossterm::{
@@ -11,19 +10,34 @@ use crossterm::{
 use ratatui::{
     backend::CrosstermBackend,
     layout::{Constraint, Layout, Rect},
-    widgets::{canvas::Rectangle, Block, Borders, Padding},
+    style::Stylize,
+    text::{self, Line, Span},
+    widgets::{canvas::Rectangle, Block, Borders, Padding, Paragraph, Wrap},
     Terminal,
 };
 
 use rand::{rngs::OsRng, seq::SliceRandom};
 use tui_textarea::{Input, Key, TextArea};
 
-
 const DATA: &'static str = include_str!("br_utf8.txt");
-fn get_words()->Vec<&'static str>{
-    let mut words: Vec<&str> = DATA.split("\n").map(|f|f.trim()).collect();
+fn get_words() -> Vec<&'static str> {
+    let mut words: Vec<&str> = DATA.split("\n").map(|f| f.trim()).collect();
     words.shuffle(&mut OsRng);
     words
+}
+
+fn get_actual_words<'a>(index: usize, words: &'a Vec<&'a str>)->Vec<Span>{
+    let max_displaying_words = 50;
+    words[index..max_displaying_words+index]
+    .into_iter()
+    .map(|f| {
+        if f.eq(&words[index]){
+            Span::raw(format!("{} ", f)).bold()
+        }else{
+            Span::raw(format!("{} ", f))
+        }
+    })
+    .collect()
 }
 
 fn main() -> Result<(), Box<(dyn Error)>> {
@@ -39,23 +53,50 @@ fn main() -> Result<(), Box<(dyn Error)>> {
             .padding(Padding::horizontal(2)),
     );
 
+    
+
     let layout = Layout::new(
         ratatui::layout::Direction::Vertical,
-        [
-            Constraint::Min(1),
-            Constraint::Length(3),
-        ]
-    ).split(terminal.size()?);
+        [Constraint::Min(1), Constraint::Length(3)],
+    )
+    .split(terminal.size()?);
+
+    let words = get_words();
+    let mut index: usize = 0;
+
+
     loop {
         terminal.draw(|f| {
+            
+            let paragraph_block = Block::new()
+            .borders(Borders::ALL)
+            .border_type(ratatui::widgets::BorderType::Thick);
+
+
+            f.render_widget(Paragraph::new(vec![get_actual_words(index, &words).into()]).wrap(Wrap{trim:true}).block(paragraph_block), layout[0]);
             f.render_widget(textarea.widget(), layout[1]);
         })?;
 
-        match crossterm::event::read()?.into(){
-            Input {key: Key::Char('m'), ctrl: true, alt: false, shift: false} | Input{key: Key::Enter, ..} => continue,
-            Input {key: Key::Char(' '),..} => continue,
-            Input {key: Key::Esc, ..} => break,
-            input=>{
+        match crossterm::event::read()?.into() {
+            Input {
+                key: Key::Char('m'),
+                ctrl: true,
+                alt: false,
+                shift: false,
+            }
+            | Input {
+                key: Key::Enter, ..
+            } => continue,
+            Input {
+                key: Key::Char(' '),
+                ..
+            } => {
+                index+=1;
+                textarea.delete_line_by_head();
+                continue
+            },
+            Input { key: Key::Esc, .. } => break,
+            input => {
                 textarea.input(input);
             }
         }
